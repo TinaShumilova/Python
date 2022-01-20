fa0af8170b7dae6922e126a159eda711eca533fe
Tina Shumilova <shutiva@gmail.com>
2022-01-20 18:32:43 +0200

    while-else

--- Lesson_1/Lesson_1.py
+++ Lesson_1/Lesson_1.py
@@ -76,9 +76,20 @@
 # print('Привет, ', username)
 
 #while
+#original = 23
+#inverted = 0
+#while original != 0:
+#    inverted = inverted * 10 + (original % 10)
+#    original //= 10
+#print(inverted)
+
+#while-else
 original = 23
 inverted = 0
 while original != 0:
     inverted = inverted * 10 + (original % 10)
     original //= 10
+else: 
+    print('Пожалуй')
+    print('хватит )')
 print(inverted)
