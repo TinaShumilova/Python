3ecc58dfd7a6c7edc9a882718aecc71f9d5b530f
Tina Shumilova <shutiva@gmail.com>
2022-01-20 16:52:05 +0200

    add str and Interpolation

--- Lesson_1/Lesson_1.py
+++ Lesson_1/Lesson_1.py
@@ -1,13 +1,20 @@
 # типы данных, переменные 
 # int, float, boolean, str, list, None
-# int
 
+# int
 a = 123
 print(a)
 # float
 b = 1.23
 print(b)
-
+# None
 value = None
 value = 1234
 print(value)
+#str
+text = 'hello \'world\''
+print(text)
+print(a, b, text)                       
+print(a, '-', b, '-', text)                
+print('{} - {} - {}'.format(a,b,text))   
+print(f'{a} - {b} - {text}')  
