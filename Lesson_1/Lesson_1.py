b5044688e4608cbe0c365dee60b3ef716ab45556
Tina Shumilova <shutiva@gmail.com>
2022-01-20 17:04:53 +0200

    add boolean and list

--- Lesson_1/Lesson_1.py
+++ Lesson_1/Lesson_1.py
@@ -2,19 +2,33 @@
 # int, float, boolean, str, list, None
 
 # int
-a = 123
-print(a)
+#a = 123
+#print(a)
 # float
-b = 1.23
-print(b)
+#b = 1.23
+#print(b)
 # None
-value = None
-value = 1234
-print(value)
+#value = None
+#value = 1234
+#print(value)
 #str
-text = 'hello \'world\''
-print(text)
-print(a, b, text)                       
-print(a, '-', b, '-', text)                
-print('{} - {} - {}'.format(a,b,text))   
-print(f'{a} - {b} - {text}')  
+#text = 'hello \'world\''
+#print(text)
+
+#print(a, b, text)                       
+#print(a, '-', b, '-', text)                
+#print('{1} - {2} - {0}'.format(a,b,text))   
+# print(f'{a} - {b} - {text}')  
+
+# boolean
+s = True
+print(s)
+#list
+list_1 = []
+list_2 = [1, 2, 3]
+list_3 = ['1', '2', '3', 'hello']
+list_4 = ['1', '2', '3', 'hello', 1.5248, True]
+print(list_1)
+print(list_2)
+print(list_3)
+print(list_4)
