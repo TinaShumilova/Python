9d81cae7a1afecfc10ea95493c374bda1ec6daf2
Tina Shumilova <shutiva@gmail.com>
2022-01-20 18:24:37 +0200

    if-elif-else

--- Lesson_1/Lesson_1.py
+++ Lesson_1/Lesson_1.py
@@ -57,10 +57,20 @@
 #print(is_odd)
 
 # if-else
+#a = int(input('a = '))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
 
-a = int(input('a = '))
-b = int(input('b = '))
-if a > b:
-    print(a)
+# if-elif-else
+username = input('Введите имя: ')
+if username == 'Маша':
+ print('Ура, это же МАША!')
+elif username == 'Марина':
+ print('Я так ждала Вас, Марина!')
+elif username == 'Ильнар':
+ print('Ильнар - топ)')
 else:
-    print(b)
+ print('Привет, ', username)
